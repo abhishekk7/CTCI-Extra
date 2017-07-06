27b3cbcdce9ff0f70553045b608eb8014049bb70
Abhishek Kumar <abhishek3433kumar@gmail.com>
2017-07-06 15:46:42 -0500

    Questions

---
+++ ConceptImpl/src/QuestionsImpl/Chapter1/IsPermutation.java
@@ -0,0 +1,30 @@
+package QuestionsImpl.Chapter1;
+
+/**
+ * Created by Abhishek Kumar on 7/6/2017.
+ */
+public class IsPermutation {
+    public static boolean isPermutation(String one, String two) {
+        if (one.length() != two.length()) return false;
+
+        int[] letters = new int[128];
+        for (int i = 0; i < one.length(); i++) {
+            letters[one.charAt(i)]++;
+        }
+
+        for (int i = 0; i < two.length(); i++) {
+            letters[two.charAt(i)]--;
+            if (letters[two.charAt(i)] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[][] pairs = {{"apple", "papel" }, {"carrot", "tarroc" }, {"hello", "llloh" }};
+        for (String[] pair : pairs) {
+            System.out.println(pair[0] + ", " + pair[1] + " : " + isPermutation(pair[0], pair[1]));
+        }
+    }
+}
