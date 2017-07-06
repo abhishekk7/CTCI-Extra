f5ec4f2e337ef5088cc81eabc4fac3213feb0997
abhishekk7 <abhishek3433kumar@gmail.com>
2017-07-05 23:38:33 -0500

    First check in

---
+++ ConceptImpl/src/HashTableImpl/RunHashTable.java
@@ -0,0 +1,20 @@
+package HashTableImpl;
+
+/**
+ * Created by Abhishek Kumar on 7/5/2017.
+ */
+public class RunHashTable {
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = new Map<>();
+        map.add("this", 1);
+        map.add("coder", 2);
+        map.add("this", 4);
+        map.add("hi", 5);
+        System.out.println(map.size());
+        System.out.println(map.remove("this"));
+        System.out.println(map.remove("this"));
+        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+    }
+}
